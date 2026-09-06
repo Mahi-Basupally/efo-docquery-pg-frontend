e276f553a8fcdf5e43808d17a4da2502f0d4f20e
Mahi-Basupally <bhelmahi@gmail.com>
2026-09-06 13:13:13 -0400

    Add F3P API data contract

---
+++ src/lib/api/f3p.ts
@@ -0,0 +1,4 @@
+import type { FinancialReportData } from '@/components/forms/FinancialReportRenderer';
+
+export type F3PReportData = FinancialReportData;
+export type F3PReportResponse = { data: F3PReportData };
